741b825ba44f04d3fafdb95cad835e8734b6a684
Jesse C. Owens <jesse@jessecowens.com>
2019-05-01 13:53:25 -0600

    Fix Text domain

--- jco-asciinema-player.php
+++ jco-asciinema-player.php
@@ -21,7 +21,7 @@
  * Author URI:        https://jessecowens.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       jco
+ * Text Domain:       jco-asciinema-player
  * Domain Path:       /languages
  *
  * JCO Asciinema Player is free software: you can redistribute it and/or modify
